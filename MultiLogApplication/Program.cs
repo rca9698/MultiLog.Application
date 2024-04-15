a7f770f80cf2e7800ecf49b0859a30914141e697
rca <rca>
2024-04-16 03:54:39 +0530

    code updated

--- MultiLogApplication/Program.cs
+++ MultiLogApplication/Program.cs
@@ -7,8 +7,19 @@ var config = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(x => x.LoginPath = "/Home/Index");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.ExpireTimeSpan = TimeSpan.FromDays(3650); // Set the expiration time to a very large value (e.g., 10 years)
+    options.SlidingExpiration = true; // Optionally, enable sliding expiration to extend the expiration time with each request
+    options.LoginPath = "/Home/Index";
+});
 
 builder.Services.AddSession(options =>
 {
